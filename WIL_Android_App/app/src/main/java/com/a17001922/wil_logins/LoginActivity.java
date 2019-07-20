c9e935d7d8fcc6b0cb0711d7e02765a125eeef0c
Wade Buckthorp <Wbuckthorp@gmail.com>
2019-07-20 22:26:49 +0200

    First commit with logins

---
+++ WIL_Android_App/app/src/main/java/com/a17001922/wil_logins/LoginActivity.java
@@ -0,0 +1,63 @@
+package com.a17001922.wil_logins;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class LoginActivity extends AppCompatActivity {
+    Button btnLogin;
+    TextView lblRegister;
+    EditText et_email,et_password;
+    LoginUser user;
+    Connection con;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        user = new LoginUser();
+        con=new Connection();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        btnLogin=findViewById(R.id.btn_login);
+        lblRegister =findViewById(R.id.txt_register);
+        et_email=findViewById(R.id.et_userName);
+        et_password=findViewById(R.id.et_Password);
+        //#TODO Create the login request in back end class and send the intent to the home page
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = et_email.getText().toString();
+                String password=et_password.getText().toString();
+                user.setEmail(email);
+                user.setPassword(password);
+                String message="";
+                if(con.userLogin(user)==false){
+                    message="false";
+                }else{
+                    message="true";
+                }
+                Toast.makeText(LoginActivity.this,message , Toast.LENGTH_LONG).show();
+            }
+        });
+        lblRegister.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Intent i = new Intent(getApplicationContext(),RegisterActivity.class);
+                startActivity(i);
+                return false;
+            }
+        });
+
+
+    }
+}
---
+++ WIL_Android_App/app/src/main/java/com/a17001922/wil_logins/LoginUser.java
@@ -0,0 +1,24 @@
+package com.a17001922.wil_logins;
+/*
+#TODO THIS IS THE LOGIN MODEL FOR USERS THAT WE USE ANY INCLUDED FILEDS ON THE DATABASE SIDE WILL NEED TO BE ADDED INTO THIS CLASS AS WELL
+ */
+public class LoginUser {
+    protected String email;
+    protected String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
