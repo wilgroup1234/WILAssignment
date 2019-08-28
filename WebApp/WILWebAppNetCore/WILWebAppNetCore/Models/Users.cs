d77c72ac3335aaa5171b003f1d95ffa6996289cb
Jarrod <jarrodsingh1234@gmail.com>
2019-08-28 19:09:04 +0200

    updated models

--- WebApp/WILWebAppNetCore/WILWebAppNetCore/Models/Users.cs
+++ WebApp/WILWebAppNetCore/WILWebAppNetCore/Models/Users.cs
@@ -16,7 +16,6 @@ namespace WILWebAppNetCore.Models
         public int UserId { get; set; }
         public string LastName { get; set; }
         public string FirstName { get; set; }
-        public int Age { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
 
